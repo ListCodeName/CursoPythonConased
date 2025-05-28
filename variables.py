93f864c862e70f5bf4fd34f6be881612568550ff
listcodename <angelzero16@hotmail.com>
2025-05-28 18:02:04 -0300

    Clase 1- Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+#Numero enteros 
+num1 = 10
+
+#Numeros flotantes
+flo1 = 10.5
+
+#String
+cadena = "Hola, Python!"
+
+
+#Booleanos
+booleano = True
+
+numero = input("Ingrese un número: ")
+print(numero + str(num1))
+
+#-----------------------
+# Listas 
+#-----------------------
+
+#git clone https://github.com/ListCodeName/CursoPythonConased.git
+#git fetch
+#git pull
