0f1d2239f08a67f5db623d1f4b7cd22d33e1cc04
listcodename <angelzero16@hotmail.com>
2025-05-29 13:18:33 -0300

    Ejercicios variables y operadores

---
+++ Práctica/ejercicio1.py
@@ -0,0 +1,25 @@
+# ========================
+# Variables y operadores
+# ========================
+# Ejercicio 1:
+# Crea un programa que declare dos variables, una con tu nombre y otra con tu edad.
+# Muestrelos por pantalla de la siguiente forma:
+# "Hola, me llamo Juan y tengo 20 años."
+#
+# Ejercicio 2:
+# Ingrese dos números por teclado y muestre por pantalla cual es el mayor de los dos.
+#
+# Ejercicio 3:
+# Ingrese 3 números por teclado y muestre por pantalla el mayor de los tres.
+#
+# Ejercicio 4:
+# Ingrese 3 números por teclado y muestrelos ordenados de menor a mayor.
+#
+# Ejercicio 5:
+# Ingrese un número por teclado y muestre por pantalla si es par o impar.
+#
+# Ejercicio 6:
+# Ingrese el precio de un producto y el porcentaje de descuento. Al final, muestre por pantalla 
+# el precio final del producto con el descuento aplicado, el descuento que se hizo, y el precio original.
+# Ej: Precio final: 80.0, Descuento: 20.0%, Precio original: 100.0
+
