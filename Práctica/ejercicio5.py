eb7ef63086ec7d6978351bce0deee46293a9c73b
listcodename <angelzero16@hotmail.com>
2025-06-13 17:28:02 -0300

    Ejercicio 5

---
+++ Práctica/ejercicio5.py
@@ -0,0 +1,5 @@
+# Ejercicio de de Listas y diccionarios
+# Crea un programa que almacene en una lista que almacene los datos de un atleta olimpico.
+# El programa debe pedir al usuario que introduzca el nombre del atleta, la edad, la disciplina que practica y el pais de origen.
+# El programa debe almacenar los datos en un diccionario y añadirlo a la lista.
+# El programa deberá también mostrar el mejor atleta por cada disciplina.
